0ed55a3b8386111a053512154a3625258e2e8ce0
benhur <binhamster3@gmail.com>
2026-08-09 13:56:38 +0800

    better it commit

--- app.py
+++ app.py
@@ -175,3 +175,5 @@ def send_email(to, subject, template, **kwargs):
     msg.body = render_template(template + ".txt", **kwargs)
     msg.html = render_template(template + ".html", **kwargs)
     mail.send(msg)
+
+ # 20260809 test
